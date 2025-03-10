1c1599d82609b64232735987815f0dbc89b6c331
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4a6c9.42berlin.de>
2025-03-10 23:41:42 +0100

    100%

---
+++ PROJECTS/C06/ex02/ft_rev_params.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 10:00:04 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2025/03/07 10:06:49 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	parameters;
+
+	parameters = argc - 1;
+	while (parameters > 0)
+	{
+		ft_putstr(argv[parameters]);
+		ft_putchar('\n');
+		parameters--;
+	}
+	return (0);
+}
